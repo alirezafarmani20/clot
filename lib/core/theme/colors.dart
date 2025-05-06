a7f292bebd317a4f6c1119a29e4630b9af31f9d4
alireza <farmanialireza668@gmail.com>
2025-05-06 21:47:21 +0330

    set project color

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff8E6CEF);
+  static const Color appBlack = Color(0xff272727);
+  static const Color bgLight1 = Color(0xffFFFFFF);
+  static const Color bgLight2 = Color(0xffF4F4F4);
+}
+
+// dark mode colors
+
+class AppColorDarck {
+    static const Color primary = Color(0xff8E6CEF);
+    static const Color bgDark1 = Color(0xff1D182A);
+    static const Color bgDark2 = Color(0xff342F3F);
+    static const Color white50 = Color(0xffFFFFFF);
+    static const Color white100 = Color(0xffFFFFFF);
+}
