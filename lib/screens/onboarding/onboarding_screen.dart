d5f6483cffdff50226b3a3be727afdec22e644b0
alireza <farmanialireza668@gmail.com>
2025-05-07 13:46:53 +0330

    create onboarding screen

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -1,3 +1,5 @@
+import 'package:clot/core/theme/colors.dart';
+import 'package:clot/screens/register/singin_screen.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -9,12 +11,23 @@ class OnboardingScreen extends StatefulWidget {
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SinginScreen()),
+      );
+    });
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.primary,
       body: Center(
         // ignore: avoid_unnecessary_containers
         child: Container(
-          child: Image(image: AssetImage("")),
+          child: Image(image: AssetImage("lib/assets/images/logo.png"))
         ),
       )
     );
---
+++ lib/screens/register/singin_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class SinginScreen extends StatefulWidget {
+  const SinginScreen({super.key});
+
+  @override
+  State<SinginScreen> createState() => _SinginScreenState();
+}
+
+class _SinginScreenState extends State<SinginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
